2976194c2e2d65cdfb7a04b935d2ecfc5f0dfc2f
Ahmed Abdallatif <136247970+AhmedElian@users.noreply.github.com>
2025-08-11 19:44:28 +0300

    Add files via upload

---
+++ UserAuthentication.java
@@ -0,0 +1,13 @@
+package AssignmentsTestNG;
+
+public class UserAuthentication {
+
+    public boolean authenticate(String username, String password) {
+        // Dummy authentication logic
+        if ("admin".equals(username) && "password123".equals(password)) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+    }
